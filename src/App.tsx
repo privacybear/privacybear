3ffb8f44750649f5f76f9d680289fbc16aefe673
Michael55555 <michaelpeters5647@gmail.com>
2020-07-28 08:53:03 +0200

    Add settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React, { Component } from 'react'
-import { Home } from './components/Home';
-import { Login, Register } from './components/Auth/auth';
-import { createBrowserHistory } from 'history';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+import React, { Component } from "react";
+import { Home } from "./components/Home";
+import { Login, Register } from "./components/Auth/auth";
+import { createBrowserHistory } from "history";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Settings from "./components/Settings/settings";
 
 const history = createBrowserHistory();
 
@@ -16,15 +13,16 @@ export class App extends Component {
       <div>
         <Router>
           <Switch>
+            <Route exact path="/settings" component={Settings} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/" component={Home} />
-            <Route path="*">HAHAHA</Route>
+            <Route path="*">040 - Requested URL not found</Route>
           </Switch>
         </Router>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/components/Settings/Config.ts
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../Auth/Layout";
+
+export function Config() {
+  return (
+    <Layout>
+      <div>Hey</div>
+    </Layout>
+  );
+}
+
+export default Config;
